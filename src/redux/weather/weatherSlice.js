aab0ad1e3c367776f3e2d3afe41ac219d5497a4c
Reza Merzaie <ghrezam97@gmail.com>
2023-08-01 04:35:13 +0430

    Add weather slice and reducer

---
+++ src/redux/weather/weatherSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getWeather = createAsyncThunk(
+  'countryStates/getWeather',
+  async (stateName) => {
+    const key = 'b134e251af58430f964152538220407';
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${stateName}&days=3&aqi=yes`,
+    );
+    const data = await response.json();
+    return data;
+  },
+);
+
+const initialState = {
+  weatherDetails: {},
+  isLoading: false,
+};
+
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getWeather.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getWeather.fulfilled, (state, action) => {
+        state.weatherDetails = action.payload;
+      });
+  },
+});
+
+export default weatherSlice.reducer;
