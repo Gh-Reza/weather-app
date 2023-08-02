773c169830f732526598f64e250d792c4871f22f
Reza Merzaie <ghrezam97@gmail.com>
2023-08-02 22:07:49 +0430

    Remove bootstrap.js/

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
