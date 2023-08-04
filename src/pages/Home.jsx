bf843c2a8ddce5380997907e3ec956436c3aa61b
Reza Merzaie <ghrezam97@gmail.com>
2023-08-04 13:46:16 +0430

    Refactore the Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { getCountries } from '../redux/countries/countriesSlice';
 import Country from '../components/country';
 import { getCountryStates } from '../redux/countries/statesSlice';
+import Search from '../components/Search';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,20 +18,23 @@ const Home = () => {
 
   const countries = useSelector((store) => store.countries.countries);
   return (
-    <main className="country-container container container-md-fluid">
-      <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4">
-        {countries.map((country, i) => (
-          <Country
-            key={country.name}
-            name={country.name}
-            flag={country.flag}
-            capital={country.capital[0]}
-            lat={country.latlng[0]}
-            lng={country.latlng[1]}
-            onClick={handleClick}
-            index={i}
-          />
-        ))}
+    <main className="container container-md-fluid">
+      <Search />
+      <div className="container country-container">
+        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4">
+          {countries.map((country, i) => (
+            <Country
+              key={country.name}
+              name={country.name}
+              flag={country.flag}
+              capital={country.capital[0]}
+              lat={country.latlng[0]}
+              lng={country.latlng[1]}
+              onClick={handleClick}
+              index={i}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
