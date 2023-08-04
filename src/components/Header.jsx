6b13a53100133e91ad3d591f4e01ba9331202af3
Reza Merzaie <ghrezam97@gmail.com>
2023-08-04 13:45:57 +0430

    Refactore the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import Icon from '../assets/icon.png';
 import BackIcon from '../assets/icon50.png';
-import Search from './Search';
 
 const Header = () => {
   const location = useLocation();
   const isDetailsPage = location.pathname === '/details';
   return (
     <header className="header text-white d-flex justify-content-between align-items-center">
-      <div className="left d-flex align-items-center">
+      <div className="d-flex">
         {isDetailsPage && <NavLink to="/"><img src={BackIcon} alt="Back icon" className="back-icon me-4" /></NavLink> }
+        <p className="h2">{new Date().getFullYear()}</p>
+      </div>
+      <div className="left d-flex align-items-center">
         <h1 className="fs-1">WeatherAPP</h1>
       </div>
       <div className="right">
-        {!isDetailsPage && <Search />}
         <img src={Icon} alt="" />
       </div>
     </header>
